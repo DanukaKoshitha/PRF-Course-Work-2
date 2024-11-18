76af2973b181ebbf360b7b20a24f925fc8155bb5
Danuka koshitha <danukakoshitha2004@gmail.com>
2024-11-18 07:23:59 +0530

    first commit

---
+++ Example.java
@@ -0,0 +1,167 @@
+import java.util.*;
+class Example{
+	public static void main(String args[]){
+	Scanner input=new Scanner(System.in);
+	System.out.println("\r\n                                           \r\n                                         _____                      _     ______        _     _             \r\n                                        / ____|                    | |   |  ____|      | |   (_)            \r\n                                       | (___  _ __ ___   __ _ _ __| |_  | |__ __ _ ___| |__  _  ___  _ __  \r\n                                        \\___ \\| \'_ ` _ \\ / _` | \'__| __| |  __/ _` / __| \'_ \\| |/ _ \\| \'_ \\ \r\n                                        ____) | | | | | | (_| | |  | |_  | | | (_| \\__ \\ | | | | (_) | | | |\r\n                                       |_____/|_| |_| |_|\\__,_|_|   \\__| |_|  \\__,_|___/_| |_|_|\\___/|_| |_|\r\n                                                                      \r\n                                                                      \r\n\r \r   \r");
+
+
+	System.out.print("				       Date :");
+        String x=input.next();
+System.out.println("\n");
+	System.out.print("				       Customer Name :");
+        String y=input.next();
+	System.out.println("                                       _____________________________________________________________________");
+
+	System.out.println("\r\n                        \r\n                                                    _______     _____ _     _      _       \r\n                                                   |__   __|   / ____| |   (_)    | |      \r\n                                                      | |_____| (___ | |__  _ _ __| |_ ___ \r\n                                                      | |______\\___ \\| \'_ \\| | \'__| __/ __|\r\n                                                      | |      ____) | | | | | |  | |_\\__ \\\r\n                                                      |_|     |_____/|_| |_|_|_|   \\__|___/\r\n                                         \r                                         \r");              
+
+System.out.println("\n");
+
+	System.out.print("				       Number of T-Shirts  :");
+       int z=input.nextInt();
+System.out.println("\n");
+	System.out.print("				       Unit Price of T-Shirts :");
+        double w=input.nextDouble();
+System.out.println("\n");
+	System.out.print("				       Discount Rate(%) :");
+        double e=input.nextDouble();
+System.out.println("\n");
+double t=(w-(w*e/100))*z
+;
+	System.out.println("                                      +--------------------------------------------------------------------+");
+System.out.println("				      | Description       | QTY            |   Unit Price        |  Amount | ");
+	System.out.println("                                      +--------------------------------------------------------------------+");
+System.out.println("			              | T-Shirts          | "+z+"           |   "+w+"              |  "+t+"     |");
+	System.out.println("                                      +--------------------------------------------------------------------+");
+System.out.println("\n");
+
+	System.out.println("                                      ______________________________________________________________________");
+System.out.println("\r\n                                                       _______                                \r\n                                                      |__   __|                               \r\n                                                         | |_ __ ___  _   _ ___  ___ _ __ ___ \r\n                                                         | | \'__/ _ \\| | | / __|/ _ \\ \'__/ __|\r\n                                                         | | | | (_) | |_| \\__ \\  __/ |  \\__ \\\r\n                                                         |_|_|  \\___/ \\__,_|___/\\___|_|  |___/\r\n                                         \r                                         \r");
+
+
+
+System.out.println("\n");
+
+	System.out.print("				       Number of Trousers  :");
+       int r=input.nextInt();
+System.out.println("\n");
+	System.out.print("				       Unit Price of Trousers :");
+        double u=input.nextDouble();
+System.out.println("\n");
+	System.out.print("				       Discount Rate(%) :");
+        double i=input.nextDouble();
+System.out.println("\n");
+
+
+double c=(u-(u*i/100))*r;
+	System.out.println("                                      +--------------------------------------------------------------------+");
+System.out.println("				      | Description       | QTY            |   Unit Price        |  Amount | ");
+	System.out.println("                                      +--------------------------------------------------------------------+");
+System.out.println("				      | Trousers          | "+r+"           |   "+u+"              |  "+c+"      |");
+	System.out.println("                                      +--------------------------------------------------------------------+");
+System.out.println("\n");
+
+	System.out.println("                                      ______________________________________________________________________");
+
+
+
+
+
+
+
+System.out.println("\r\n                                                    \r\n                                                             _____ _     _      _       \r\n                                                            / ____| |   (_)    | |      \r\n                                                           | (___ | |__  _ _ __| |_ ___ \r\n                                                            \\___ \\| \'_ \\| | \'__| __/ __|\r\n                                                            ____) | | | | | |  | |_\\__ \\\r\n                                                           |_____/|_| |_|_|_|   \\__|___/\r\n                              \r                              \r");
+
+
+
+System.out.println("\n");
+
+	System.out.print("				       Number of Shirts  :");
+       int o=input.nextInt();
+System.out.println("\n");
+	System.out.print("				       Unit Price of Shirts :");
+        double p=input.nextDouble();
+System.out.println("\n");
+	System.out.print("				       Discount Rate(%) :");
+        double l=input.nextDouble();
+System.out.println("\n");
+
+double n=(p-(p*l/100))*o;
+	System.out.println("                                      +--------------------------------------------------------------------+");
+System.out.println("				      | Description     | QTY             |   Unit Price        |  Amount  | ");
+	System.out.println("                                      +--------------------------------------------------------------------+");
+System.out.println("				      | Shirts          | "+o+"           |   "+p+"              |  "+n+"      |");
+	System.out.println("                                      +--------------------------------------------------------------------+");
+System.out.println("\n");
+
+	System.out.println("                                      ______________________________________________________________________");
+
+System.out.println("\r\n                                               \r\n                                                          _____ _                _       \r\n                                                         / ____| |              | |      \r\n                                                        | (___ | |__   ___  _ __| |_ ___ \r\n                                                         \\___ \\| \'_ \\ / _ \\| \'__| __/ __|\r\n                                                         ____) | | | | (_) | |  | |_\\__ \\\r\n                                                        |_____/|_| |_|\\___/|_|   \\__|___/\r\n                                  \r                                  \r     ");
+
+
+
+
+System.out.println("\n");
+
+	System.out.print("				       Number of Shorts  :");
+       int k=input.nextInt();
+System.out.println("\n");
+	System.out.print("				       Unit Price of Shorts :");
+        double j=input.nextDouble();
+System.out.println("\n");
+	System.out.print("				       Discount Rate(%) :");
+        double b=input.nextDouble();
+System.out.println("\n");
+
+double m=(j-(j*b/100))*k;
+	System.out.println("                                      +--------------------------------------------------------------------+");
+System.out.println("				      | Description     | QTY             |   Unit Price        |  Amount | ");
+	System.out.println("                                      +--------------------------------------------------------------------+");
+System.out.println("				      | Shorts          | "+k+"           |   "+j+"              |  "+m+"      |");
+	System.out.println("                                      +--------------------------------------------------------------------+");
+System.out.println("\n");
+
+	System.out.println("                                       ______________________________________________________________________");
+System.out.println("\n");
+
+
+	System.out.println("                                       +--------------------------------------------------------------------+");
+
+
+
+
+System.out.println("\r                                               \r                                          \r                                       |   ____  _ _ _    _____                                             |\r\n                                       |  |  _ \\(_) | |  / ____|                                            |\r\n                                       |  | |_) |_| | | | (___  _   _ _ __ ___  _ __ ___   __ _ _ __ _   _  |\r\n                                       |  |  _ <| | | |  \\___ \\| | | | \'_ ` _ \\| \'_ ` _ \\ / _` | \'__| | | | |\r\n                                       |  | |_) | | | |  ____) | |_| | | | | | | | | | | | (_| | |  | |_| | |\r\n                                       |  |____/|_|_|_| |_____/ \\__,_|_| |_| |_|_| |_| |_|\\__,_|_|   \\__, | |\r\n                                       |                                                              __/ | |\r\n                                       |                                                             |___/  |\r\n                                       |                                                                    |");
+
+
+
+
+
+System.out.println("                                       |  Customer : "+y+"                               Date : "+x+"  |");
+
+System.out.println("                                       |  								    |");
+
+	System.out.println("                                       +--------------------------------------------------------------------+");
+
+System.out.println("				       | Description     | QTY            |   Unit Price        |  Amount   | ");
+
+	System.out.println("                                       +--------------------------------------------------------------------+");
+
+System.out.println("				       |                 |                |                     |           |");
+System.out.println("				       | T-Shirts        |    "+z+"          |        "+w+"        |    "+t+"       |");
+System.out.println("				       |                 |                |                     |           |");
+System.out.println("			  	       | Trousers        |    "+r+"           |       "+u+"              |  "+c+"      |");
+System.out.println("				       |                 |                |                     |           |");
+System.out.println("			    	       | Shirts          |    "+o+"           |       "+p+"              |  "+n+"      |");
+System.out.println("				       |                 |                |                     |           |");
+System.out.println("				       | Shorts          |    "+k+"           |       "+j+"              |  "+m+"      |");
+System.out.println("				       |                 |                |                     |           |");
+
+
+	System.out.println("                                       +--------------------------------------------------------------------+");
+
+
+System.out.println("				       |  Total           					| "+(t+c+n+m)+"  |");
+	System.out.println("                                       +--------------------------------------------------------------------+");
+
+
+
+}
+}          							
